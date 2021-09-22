fa2c5a0a2624d43ea33d9b7be9312d19d7c21f61
Trusha Patel <91124823+trusha-ce@users.noreply.github.com>
2021-09-22 12:13:42 +0530

    Update mail.cpp

--- mail.cpp
+++ mail.cpp
@@ -20,6 +20,8 @@ int main()
     NEXT:
 
     system("CLS");
+
+    //-------------------------print main menu (M)
     line();
     cout<<"S : Sign up"<<endl;
     cout<<"L : Login"<<endl;
@@ -28,6 +30,7 @@ int main()
     cout<<"Enter choice : ";
     cin>>choice;
 
+    //-------------------------menu choice
     switch(choice)
     {
     case 'S':
@@ -43,16 +46,20 @@ int main()
         goto NEXT;
     }
 
+    //-------------------------exit program on E choice
     E:
     return 0;
 }
 
+//-------------------------function of sign up
 void signup()
 {
     ofstream fout;
     string fname,id,pass;
 
     system("CLS");
+
+    //-------------------------ask for user id & password for sign up
     line();
     cout<<"New user registration";
     line();
@@ -61,22 +68,28 @@ void signup()
     cout<<setw(12)<<"Password : ";
     cin>>pass;
 
+    //-------------------------append user id & password in user.txt file
     fout.open("user.txt",ios::app);
     fout<<id<<'\t'<<pass<<endl;
     fout.close();
 
+    //-------------------------create userid_s.txt file
     fname=id+"_s.txt";
 
     fout.open(fname.c_str(),ios::out);
     fout.close();
 
+    //-------------------------create userid_r.txt file
     fname=id+"_r.txt";
 
     fout.open(fname.c_str(),ios::out);
     fout.close();
 
+    //-------------------------call home screen function
     home(id);
 }
+
+//-------------------------function of login
 void login()
 {
     ifstream fin;
@@ -85,6 +98,8 @@ void login()
     char next;
 
     system("CLS");
+
+    //-------------------------ask for user if & password for login
     line();
     cout<<"User Login";
     line();
@@ -93,6 +108,7 @@ void login()
     cout<<setw(12)<<"Password : ";
     cin>>pass;
 
+    //-------------------------validate user id & password with entry in user.txt file
     fin.open("user.txt",ios::in);
     while(!fin.eof())
     {
@@ -104,6 +120,8 @@ void login()
         }
     }
     fin.close();
+
+    //-------------------------invalid credential print error message
     if(found==0)
     {
         cout<<endl<<"***** ERROR : invalid user ID or password. *****"<<endl<<"press 'y' to try again, press 'n' to exit : ";
@@ -117,12 +135,14 @@ void login()
             main();
         }
     }
+    //-------------------------valid credential call home screen function
     else
     {
             home(id);
     }
 }
 
+//-------------------------function of home screen
 void home(string id)
 {
     char choice;
@@ -130,10 +150,13 @@ void home(string id)
     NEXT:
 
     system("CLS");
+
+    //-------------------------print login user name on top
     line();
     cout<<"Hello "<<id<<"...";
     line();
 
+    //-------------------------print user menu (U)
     cout<<"C : Compose mail"<<endl;
     cout<<"I : Inbox"<<endl;
     cout<<"S : Sent"<<endl;
@@ -141,6 +164,7 @@ void home(string id)
     cout<<"Enter choice : ";
     cin>>choice;
 
+    //-------------------------menu choice
     switch(choice)
     {
     case 'C':
@@ -160,6 +184,7 @@ void home(string id)
     }
 }
 
+//-------------------------function of compose mail
 void compose(string id)
 {
     ifstream fin;
@@ -171,13 +196,17 @@ void compose(string id)
     NEXT:
 
     system("CLS");
+
+    //-------------------------print login user name on top
     line();
     cout<<"Hello "<<id<<"...";
     line();
 
+    //-------------------------ask for user id in "To"
     cout<<"To : ";
     cin>>to;
 
+    //-------------------------validate user id with entry in user.txt file
     fin.open("user.txt",ios::in);
     while(!fin.eof())
     {
@@ -189,6 +218,8 @@ void compose(string id)
         }
     }
     fin.close();
+
+    //-------------------------invalidate user id print error message
     if(found==0)
     {
         cout<<endl<<"ERROR : receiver does not exist."<<endl<<"press 'y' to try new user, press 'n' to exit : ";
@@ -203,6 +234,8 @@ void compose(string id)
             home(id);
         }
     }
+    //-------------------------validate user id make entry in
+    //-------------------------userid_s.txt file of sender and userid_r.txt file of receiver with content
     else
     {
         fnamer=to+"_r.txt";
@@ -228,10 +261,12 @@ void compose(string id)
         foutr.close();
         fouts.close();
 
+        //-------------------------call home screen function
         home(id);
     }
 }
 
+//-------------------------function of inbox
 void inbox(string id)
 {
     ifstream fin;
@@ -240,10 +275,13 @@ void inbox(string id)
     char next;
 
     system("CLS");
+
+    //-------------------------print login user name on top
     line();
     cout<<"Hello "<<id<<"...";
     line();
 
+    //-------------------------print content of userid_r.txt
     fname=id+"_r.txt";
     fin.open(fname.c_str(),ios::in);
     if(fin.is_open())
@@ -264,14 +302,17 @@ void inbox(string id)
         fin.close();
     }
 
-    cout<<endl<<"Press 'b' to go to home page : ";
+    cout<<endl<<"Press any key to go to home page : ";
     cin>>next;
+
+    //-------------------------go back to home screen
     if(next)
     {
         home(id);
     }
 }
 
+//-------------------------function of sent mail
 void sent(string id)
 {
     ifstream fin;
@@ -280,10 +321,13 @@ void sent(string id)
     char next;
 
     system("CLS");
+
+    //-------------------------print login user name on top
     line();
     cout<<"Hello "<<id<<"...";
     line();
 
+    //-------------------------print content of userid_s.txt
     fname=id+"_s.txt";
     fin.open(fname.c_str(),ios::in);
     if(fin.is_open())
@@ -304,18 +348,23 @@ void sent(string id)
         fin.close();
     }
 
-    cout<<endl<<"Press 'b' to go to home page : ";
+    cout<<endl<<"Press any key to go to home page : ";
     cin>>next;
+
+    //-------------------------go back to home screen
     if(next)
     {
         home(id);
     }
 }
 
+//-------------------------function to print --- line as separator
 void line()
 {
     int i;
     cout<<endl;
+
+    //-------------------------print 100 -
     for(i=0;i<100;i++)
     {
         cout<<"-";
